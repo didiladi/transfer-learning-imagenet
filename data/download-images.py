e7a6dcbf9bde3c3540417ad38aaacb15ba01b469
Dieter Ladenhauf <ladenhauf@protonmail.com>
2018-05-06 14:50:24 +0200

    Extract bounding box *.tar.gz files in script

--- data/download-images.py
+++ data/download-images.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import time
 import re
 from urllib.parse import urljoin
+import tarfile
 
 MAPPING_API = 'http://www.image-net.org/api/text/imagenet.synset.geturls.getmapping?wnid='
 BOUNDING_BOX_API = 'http://www.image-net.org/api/download/imagenet.bbox.synset?wnid='
@@ -19,6 +20,8 @@ def read_labels(file_name):
 
     E.g. [['n07745940', 'strawberry'], ['n03947888', 'pirate'], ['n02797295', 'barrow']]
     """
+
+    print("Reading desired labels")
 
     label_file = open(file_name, 'r')
     lines = label_file.readlines()
@@ -36,7 +39,7 @@ def read_labels(file_name):
 def download_bounding_boxes(labels):
     """ Downloads the boundig boxes from the imagenet website
 
-    This function downloads the files to './data/<wnid>.tar.gz', <wnid> is the internal
+    This function downloads the files to './<wnid>.tar.gz', <wnid> is the internal
     id of the given label (e.g. strawberry has wnid 'n07745940')
 
     Since imagenet uses meta refreshes, the code is capable of following the redirect
@@ -45,13 +48,17 @@ def download_bounding_boxes(labels):
     ATTENTION: Files which were already downloaded before are automatiaclly skipped.
     """
 
+    print("Downloading bounding boxes")
+
+    file_names = []
+
     for element in labels:
 
         wnid, label = element
         meta_refresh_regex = re.compile(
             '<meta[^>]*?url=(.*?)["\']', re.IGNORECASE)
 
-        file_name = './data/' + wnid + '.tar.gz'
+        file_name = './' + wnid + '.tar.gz'
         my_file = Path(file_name)
 
         # to prevent downloading the same file multiple times:
@@ -66,19 +73,46 @@ def download_bounding_boxes(labels):
                 url = urljoin(url, match.groups()[0].strip())
                 response = requests.get(url)
 
-            print("Getting bounding box for " + label +
-                  " http status code: " + str(response.status_code))
+            print("   Getting bounding box for '" + label +
+                  "', http status code: " + str(response.status_code))
 
             with open(file_name, 'wb') as f:
                 f.write(response.content)
 
+            file_names.append(file_name)
+
             # We want to be good citizens:
             time.sleep(5)
+
+    return file_names
+
+
+def extract_bounding_boxes(downloaded_files):
+    """ Extracts the bounding box files (*.tar.gz) """
+
+    print("Extracting bounding boxes")
+
+    for fname in downloaded_files:
+
+        if (fname.endswith("tar.gz")):
+            tar = tarfile.open(fname, "r:gz")
+            tar.extractall()
+            tar.close()
+        elif (fname.endswith("tar")):
+            tar = tarfile.open(fname, "r:")
+            tar.extractall()
+            tar.close()
+
+
+def download_image_files(labels):
+    print("Downloading image files")
 
 
 if __name__ == '__main__':
 
-    labels = read_labels('data/labels.txt')
-    download_bounding_boxes(labels)
+    labels = read_labels('labels.txt')
 
-    print(labels)
+    downloaded_files = download_bounding_boxes(labels)
+    extract_bounding_boxes(downloaded_files)
+
+    download_image_files(labels)
